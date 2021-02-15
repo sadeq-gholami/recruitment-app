2d94c3de202ba59bd21827e12d4d9f0dbe582525
rosapuman <amanda.heynes@gmail.com>
2021-02-15 07:05:55 +0100

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,29 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { Route, Router } from "react-router-dom";
+import Home from "./view/Home";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello world!</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      title: "Recruitment App"
+    };
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Hello world!</p>
+          <Router>
+            <Route exact path="/" component={Home} />
+          </Router>
+        </header>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
---
+++ frontend/src/view/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+//import "./Welcome.css";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Welcome">
+        <p>Welcome</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
