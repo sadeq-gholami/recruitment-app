e0e1d540ade600b8acbfe514c200248b0266e4e5
rosapuman <amanda.heynes@gmail.com>
2021-02-16 17:21:53 +0100

    applicant first page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
         </header>
         
           <Router>
-            <Route exact path="/"  exact render={(props)=>{return <ApplicantFirstPage {...props}/>}}/>
+            <Route exact path="/"  exact render={(props)=>{return <Home {...props}/>}}/>
           </Router>
 
           <footer className = "footer">
@@ -35,7 +35,6 @@ class App extends Component {
       </div>
       
     );
-     
   }
  
 }
