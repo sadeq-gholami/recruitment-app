e343328b6ed704b3bf0382694b8d48a2d2ad68f2
rosapuman <amanda.heynes@gmail.com>
2021-02-22 17:30:44 +0100

    Update ApplicantFirstDisplay.js

--- frontend/src/view/ApplicantFirstDisplay.js
+++ frontend/src/view/ApplicantFirstDisplay.js
@@ -3,57 +3,56 @@ import { Link, Route } from "react-router-dom";
 import "../style/Applicant.css";
 
 class ApplicantFirstDisplay extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-      }
+    }
 
-componentDidMount(){
-    
-}
+    componentDidMount() {
 
-  render() {
-    let competence1 = this.props.model.getCompetence();
-    let competence2 = JSON.parse(competence1)
-    let key = 0;
-    let c = competence2.map(comp =>(
-        <div key={key++} className = "competencechoice">
-            <p className = "extext">Expertise:</p>
-            <div className = "compNamediv">
-                <p className = "compName">{comp.name}</p>
-            </div>
-            <p className = "yearstext">Years of experience:</p>
-            <div className = "compYeardiv">
-                <p className = "compYear">{comp.yearsOfExperience}</p>
-            </div>
-        </div>
-    ));
-      
-    return (
-      <div className="applicantfirstdisplay-base">
-            <div className = "credtext">
-                <p>You have entered the following information:</p>
-            </div>
-          <div className="inputfielddiv">
-            <div className = "exdiv">
-                <div className = "competencediv">
-                        {c}
+    }
+
+    render() {
+        let competence1 = this.props.model.getCompetence();
+        let competence2 = JSON.parse(competence1)
+        let key = 0;
+        let c = competence2.map(comp => (
+            <div key={key++} className="competencechoice">
+                <p className="extext">Expertise:</p>
+                <div className="compNamediv">
+                    <p className="compName">{comp.name}</p>
+                </div>
+                <p className="yearstext">Years of experience:</p>
+                <div className="compYeardiv">
+                    <p className="compYear">{comp.yearsOfExperience}</p>
                 </div>
             </div>
-            
-            <div className = "savebuttondiv">
-                <Link to = "/applicantsecondpage">
-                    <button className = "savebutton">Save & continue</button>
-                </Link>
+        ));
+
+        return (
+            <div className="applicantfirstdisplay-base">
+                <div className="credtext">
+                    <p>You have entered the following information:</p>
+                </div>
+                <div className="inputfielddiv">
+                    <div className="exdiv">
+                        <div className="competencediv">
+                            {c}
+                        </div>
+                    </div>
+                    <div className="savebuttondiv">
+                        <Link to="/applicantsecondpage">
+                            <button className="savebutton">Save & continue</button>
+                        </Link>
+                    </div>
+                    <div className="savebuttondiv">
+                        <Link to="/applicantfirstpage">
+                            <button className="savebutton">Go back & edit</button>
+                        </Link>
+                    </div>
+                </div>
             </div>
-            <div className = "savebuttondiv">
-                <Link to = "/applicantfirstpage">
-                    <button className = "savebutton">Go back & edit</button>
-                </Link>
-            </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default ApplicantFirstDisplay;
