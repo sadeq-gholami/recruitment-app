d9a514a069f089d00654a1da81fd0b3637cf0165
InaRickman <43542346+InaRickman@users.noreply.github.com>
2021-03-01 09:50:43 +0100

    database migration

--- DatabaseMigration/index.js
+++ DatabaseMigration/index.js
@@ -16,13 +16,7 @@ async function main() {
 
     client.connect();
 
-    const query = `
-    SELECT *
-	FROM person;
-    `;
-    console.log(query);
-
-    let user = await client.query(query)
+    let user = await client.query('SELECT * FROM person;')
         .then(res => {
             return res;
         })
@@ -34,11 +28,44 @@ async function main() {
 
     user.rows.map(async row => {
         let role;
-        console.log(row.role_id);
-        if (row.role_id === "2") {
-            role = "applicant";
-        } else {
+        if (row.role_id === "1") {
             role = "recruiter";
+        } else {
+            role = "applicant";
+        }
+        if(row.name == null) {
+            row.name = "no data";
+        }
+        if(row.surname == null) {
+            row.surname = "no data";
+        }
+        if(row.ssn == null) {
+            row.ssn = "no data";
+        }
+        if(row.email == null) {
+            row.email = "no data";
+        }
+        if(row.role_id == null) {
+            role = "applicant";
+        }
+        if(row.username == null && row.password == null && row.email != null){
+            let username = generateRandomString();
+            let password = generateRandomString();
+            sendEmail(row.email, {username: username, password: password});
+            row.username = username;
+            row.password = password;
+        }
+        if(row.username == null) {
+            row.username = generateRandomString();
+            if(row.email != null) {
+                sendEmail(row.email, {username: row.username});
+            }
+        }
+        if(row.password == null){
+            row.password = generateRandomString();
+            if(row.email != null) {
+                sendEmail(row.email, {password: row.password});
+            }
         }
         personAry.push({
             _id: new mongoose.Types.ObjectId(),
@@ -53,6 +80,36 @@ async function main() {
         console.log(personAry);
     })
 }
+
+
+
+/**
+ * Create a random string that consists of lowercase, uppercase letters and numbers
+ * It is six characters long
+ */
+function generateRandomString() { 
+    var string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+    let randString = ''; 
+    var len = string.length; 
+    for (let i = 0; i < 6; i++ ) { 
+        randString += string[Math.floor(Math.random() * len)]; 
+    } 
+    return randString; 
+} 
+
+/**
+ * Placeholder for sending email to user
+ * @param { the email address to send to} email 
+ * @param { the data to include in the email} data 
+ */
+function sendEmail(email, data) {
+    console.log("send email to: " + email + " with data: ");
+    console.log(data);
+    //send email to user with data
+}
+ 
+
+
 
 main()
     .then(() => {
